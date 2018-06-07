f85bd3f339725ee50af7239cbd576e920c1a6443
huadong <huad@xueshandai.com>
2018-06-07 13:45:52 +0800

    bak

--- datax/report-capital_repayment.sql
+++ datax/report-capital_repayment.sql
@@ -8,7 +8,6 @@ select a.*,substring(need_repayment_time from 1 for 10 )::timestamp as need_repa
 ,capital-case when repayment_time is null then 0 else capital end as capital_unrepayment
 from repayment_detail a 
 inner join borrow b on a.borrow_id=b.id 
-inner join 
 where need_repayment_time>='2018-01-01'
 and b.status in (4,5,6)
 ;
